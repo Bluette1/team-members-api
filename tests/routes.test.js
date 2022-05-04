bb4173f8f84c76089bdbd47f9d390eb9455d0ff0
Bluette1 <marylene.sawyer@gmail.com>
2022-05-04 05:05:33 +0200

    Fix tests on ci
    issue #3

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const {app} = require('../server');
+const db = require('../app/models');
 
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({path: '.env.test'});
@@ -8,11 +9,27 @@ if (process.env.NODE_ENV === 'test') {
 let server;
 
 beforeAll((done) => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  db.mongoose
+    .connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Successfully connected to MongoDB.');
+    })
+    .catch((err) => {
+      console.error('Connection error', err);
+      process.exit();
+    });
+
   server = app.listen(4000);
   done();
 });
 
 afterAll((done) => {
+  db.mongoose.disconnect();
   server.close();
   done();
 });
@@ -23,6 +40,7 @@ describe('Auth Endpoints', () => {
       email: 'test@example.com',
       password: 'password',
     });
+    console.log('res', res);
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty('username');
   });
